a7756c261cc6cd6b064ca4a4e8adf8fd0eb115d8
Jake <jschoff155@gmail.com>
2023-10-02 10:09:38 -0500

    Add files via upload

---
+++ Digital Clock/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Digital_Clock
+{
+    partial class DigitalFormClock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ClockLabel = new System.Windows.Forms.Label();
+            this.clockTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // ClockLabel
+            // 
+            this.ClockLabel.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClockLabel.ForeColor = System.Drawing.Color.White;
+            this.ClockLabel.Location = new System.Drawing.Point(12, 9);
+            this.ClockLabel.Name = "ClockLabel";
+            this.ClockLabel.Size = new System.Drawing.Size(776, 406);
+            this.ClockLabel.TabIndex = 0;
+            this.ClockLabel.Text = "In transit. Please wait.";
+            this.ClockLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // clockTimer
+            // 
+            this.clockTimer.Interval = 1000;
+            this.clockTimer.Tick += new System.EventHandler(this.clockTimer_Tick);
+            // 
+            // DigitalFormClock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ClockLabel);
+            this.Name = "DigitalFormClock";
+            this.Text = "Digital Clock";
+            this.Load += new System.EventHandler(this.DigitalFormClock_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ClockLabel;
+        private System.Windows.Forms.Timer clockTimer;
+    }
+}
+
---
+++ Digital Clock/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Digital_Clock
+{
+    public partial class DigitalFormClock : Form
+    {
+        public DigitalFormClock()
+        {
+            InitializeComponent();
+        }
+
+        private void DigitalFormClock_Load(object sender, EventArgs e)
+        {
+            clockTimer.Start();
+        }
+
+        private void clockTimer_Tick(object sender, EventArgs e)
+        {
+            ClockLabel.Text = DateTime.Now.ToString("hh:mm:ss");
+        }
+    }
+}
